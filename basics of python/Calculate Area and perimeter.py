f9501b183067f27437a6a61fca10225d862866b9
deepikayadav97812-cpu <deepika.yadav97812@gmail.com>
2026-05-15 11:51:02 +0530

    Add files via upload

---
+++ basics of python/Calculate Area and perimeter.py
@@ -0,0 +1,6 @@
+l=int(input('enter length'))
+b=int(input('enter breadth'))
+area=l*b
+perimeter=2*(l*b)
+print('the calculated area of rectangle is', area)
+print('the calculated perimeter of rectangle is', perimeter)
